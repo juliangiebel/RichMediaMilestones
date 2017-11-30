5269f017f00c03c273c0ffc6c9bb2792dce75797
juliangiebel <juliangiebel@live.de>
2017-11-29 21:25:09 -0500

    Work on implementing the maps component

--- Meilenstein_3/src/components/gmap/gmap.ts
+++ Meilenstein_3/src/components/gmap/gmap.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
-const API_KEY = 'AIzaSyBAaL4qkuJ2fICHva1_xGAnVTvCn1HFLO8 ';
+//const API_KEY = 'AIzaSyBAaL4qkuJ2fICHva1_xGAnVTvCn1HFLO8 ';
 
+declare var google: any;
+
+//Read Doc on Map options and add relevant ones here, make options read only, 
+interface MapOptions {
+  readonly zoom: number;
+  readonly center: {lat: number, lng: number};
+}
 
 @Component({
   selector: 'gmap',
   templateUrl: 'gmap.html'
 })
-export class GmapComponent {
+export class GmapComponent implements OnInit {
 
-  text: string;
-
+  @ViewChild('map') mapElement: ElementRef;
+  
+  map: any;
+  options: MapOptions;
   constructor() {
     console.log('Hello GmapComponent Component');
-    this.text = 'Hello World';
   }
-
+  
+  ngOnInit(){
+    console.log("Ran ngOnInit in gmaps");
+    this.options = {
+      zoom: 8,
+      center: new google.maps.LatLng(43.071584, -89.380120)
+    };
+    this.initMap();
+  }
+  
+  initMap(){
+    this.map = new google.maps.Map(this.mapElement.nativeElement, this.options);
+  }
+  
 }
